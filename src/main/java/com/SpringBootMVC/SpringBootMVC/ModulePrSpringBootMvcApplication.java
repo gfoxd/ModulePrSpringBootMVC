3a06628149d0ec544b448d002c51e31d4f95ee61
Alexander gfoxd <sashanara700@gmail.com>
2026-01-27 11:56:18 +0800

    first commit

---
+++ src/main/java/com/SpringBootMVC/SpringBootMVC/ModulePrSpringBootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootMVC.SpringBootMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulePrSpringBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulePrSpringBootMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBootMVC/SpringBootMVC/ModulePrSpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootMVC.SpringBootMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulePrSpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
